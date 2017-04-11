0bc4429a01378ea34a1263f2824a0c4d63af5c9b
rainytoffee <rainytoffee@gmail.com>
2017-04-12 01:37:23 +0900

    Don't bow if you respect me.

--- lib/Junsa.rb
+++ lib/Junsa.rb
@@ -12,34 +12,26 @@ TOKEN = ENV["SLACK_API_TOKEN"]
 Slack.configure do |conf|
   conf.token = TOKEN
 end
-
 client = Slack::RealTime::Client.new
-
-# おはようポイントDBをつくる、すでにあれば開く、points変数に代入
-points = DBI.connect( 'DBI:SQLite3:junsa_points.db' )
-points.do("drop table if exists points_table")
-
-points.do("create table points_table (
-  name      varchar     not null,
-  point     int         not null,
-  state     varchar     not null
-  );")
-
 
 # slackに接続できたときの処理
 client.on :hello do
-  puts 'CCC------CCCCC----------CCCCCCCCCC-----'
+  puts 'CONNECT!'
 end
+
 
 # message eventを受け取った時の処理
 client.on :message do |data|
     you = "<@#{data['user']}>"
 
-
 cmanager = ComicManage.new
 begin
 
   case data['text']
+###########################################################
+  when /^help$/
+    client.message channel: data['channel'], text: ""
+###########################################################
 
 ###########################################################
   when /^cinit$/ #タイトル付で初期化
@@ -70,39 +62,11 @@ begin
     client.message channel: data['channel'], text: "ないよ！"
 
 ############################################################
-when /^summary (.*)$/
-  client.message channel: data['channel'], text: summary($1)
+  when /^summary (.*)$/
+    client.message channel: data['channel'], text: summary($1)
 ############################################################
-  when /おはようポイントおねがいします/
-    points.do("insert into points_table values (
-    \'#{you}\',
-    0,
-    'BUSTED'
-    );")
-    client.message channel: data['channel'], text: "おはようカウントはじめます！#{you}！"
-  when /巡査ちゃん、おはようございます/
-    points.do("update points_table set point=point+1 where name=\'#{you}\';")
-    client.message channel: data['channel'], text:"おはようございます、#{you}"
-
-    your_state = points.do("select point from points_table where name=\'#{you}\';")
-    points.do("update points_table set state='CITIZEN' where name=\'#{you}\';") if your_state > 2
-
-  when /点数教えてください/
-    your_data = points.execute("select * from points_table where name=\'#{you}\';")
-
-    your_data.each do |row|
-
-      your_point = row["point"]
-      your_state = row["state"]
-
-      client.message channel: data['channel'], text:
-      if your_point == 0
-        "ふざけるなよ"
-      else
-        "#{you}のおはようポイントは#{your_point}です！状態は#{your_state}です！"
-      end
-      end
   end
+
 rescue UncaughtThrowError => e
    client.message channel: data['channel'], text:"#{e}\n君のリストが空か、ID登録されてない。cinitした？"
 rescue SQLite3::ConstraintException => e
