d9e0fd8675a1f26749299fe43a073ad7f8a9af69
rainytoffee <rainytoffee@gmail.com>
2017-04-11 16:41:43 +0900

    oooooop

--- lib/antidote.rb
+++ lib/antidote.rb
@@ -32,7 +32,7 @@ def summary(difficulty)
     summary = Nokogiri::HTML(open("#{link}"))
     specialscore = summary.xpath("//*[@id='main-contents']/div[1]/div/div/table[1]/tbody/tr")
     weakness = summary.xpath("//*[@id='main-contents']/div[1]/div/div/table[2]/tbody/tr")
-    result << specialscore.inner_text.gsub("\n\n","\n")
+    result << specialscore.inner_text.gsub("\n\n","\n").gsub("水"," *水* ").gsub("雷"," *雷* ").gsub("氷"," *氷* ").gsub("炎"," *炎* ").gsub("聖"," *聖* ").gsub("風"," *風* ").gsub("地"," *地* ").gsub("闇"," *闇* ").gsub("毒"," *毒* ")
     result << weakness.inner_text.gsub("\n\n","\n").gsub("水"," *水* ").gsub("雷"," *雷* ").gsub("氷"," *氷* ").gsub("炎"," *炎* ").gsub("聖"," *聖* ").gsub("風"," *風* ").gsub("地"," *地* ").gsub("闇"," *闇* ").gsub("毒"," *毒* ")
     result << "----------------------------------------------\n"
 end
