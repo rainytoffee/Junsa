18bffb336667328e48000eca29c12dfbe5a666f1
Jin I <rainytoffee@gmail.com>
2017-04-24 00:06:03 +0900

    bugfix.- match roman numerals with entered numbers

--- lib/itemgen.rb
+++ lib/itemgen.rb
@@ -11,7 +11,7 @@ class Frontman
   ROMAN_NUMERALS = { "I" => 1, "II" => 2, "III" => 3, "IV" => 4, "V" => 5, "VI" => 6, "VII" => 7, "VIII" => 8, "IX" => 9, "X" => 10, "XI" => 11, "XII" => 12, "XIII" => 13, "XIV" => 14, "XV" => 15}
   def self.accept(series,query)     #front-end
       series.upcase!
-      ROMAN_NUMERALS.each { |r,n| if series.include?(r); series = "FF#{n}" end}
+      ROMAN_NUMERALS.each { |r,n| if series.match(/^#{r}$/); series = "FF#{n}" end}
       raw = Rawdata.new(series)
 
     case series
